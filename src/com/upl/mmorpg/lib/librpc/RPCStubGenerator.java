058557395002f0a2a2c975fa4b40adb01415da56
John Detter <jdetter@wisc.edu>
2016-02-27 01:38:01 -0600

    Stub Generator is working!

--- src/com/upl/mmorpg/lib/librpc/RPCStubGenerator.java
+++ src/com/upl/mmorpg/lib/librpc/RPCStubGenerator.java
@@ -22,6 +22,9 @@ public class RPCStubGenerator
 	
 	public static void main(String[] args) throws IOException 
 	{
+		String callee_receiver_object = "game";
+		String callee_prefix_args = "player_info";
+		
 		if(args.length != 1)
 		{
 			usage();
@@ -35,6 +38,16 @@ public class RPCStubGenerator
 				true, false, true);
 		FileManager callee_stubs = new FileManager(args[0] + ".callee", 
 				true, false, true);
+		FileManager handler = new FileManager(args[0] + ".handler", 
+				true, false, true);
+		
+		/* Modify the inputs for ease */
+		callee_receiver_object = callee_receiver_object + ".";
+		
+		/* Setup the basic stuff for the handler */
+		handler.println("\t\tStackBuffer result = null;");
+		handler.println("\t\tswitch(func_num)");
+		handler.println("\t\t{");
 		
 		String s;
 		int line = 0;
@@ -54,9 +67,10 @@ public class RPCStubGenerator
 				Log.e("Syntax error, line " + line);
 				Log.e("\tNot enough arguments!");
 				continue;
-			}
+			} 
 			
 			String ret_type = StackBuffer.typeSupported(parts[0]);
+			String func_name = parts[1].trim();
 			if(ret_type == null)
 			{
 				Log.e("Syntax error, line " + line);
@@ -64,21 +78,27 @@ public class RPCStubGenerator
 				continue;
 			}
 			
+			ret_type = ret_type.trim();
+			
 			String header = new String("\tpublic " 
 					+ ret_type + " "
-					+ parts[1].trim());
+					+ func_name);
 			String callee_header = new String("\tpublic " 
 					+ ret_type + " "
-					+ "__" + parts[1].trim());
+					+ "__" + func_name);
 	
-			StringBuilder arg_list = new StringBuilder("(");
-			/* Parse arguments */
+			StringBuilder prototype = new StringBuilder("(");
+			StringBuilder callee_args = new StringBuilder("("
+					+ callee_prefix_args);
+			
+			/* Parse function arguments -- do proto and callee_args */
 			String arg_types[] = new String[parts.length - 2];
 			int arg_num = 0;
 			for(arg_num = 0;arg_num < parts.length - 2;arg_num++)
 			{
+				callee_args.append(", ");
 				if(arg_num != 0)
-					arg_list.append(", ");
+					prototype.append(", ");
 				
 				int pos = arg_num + 2;
 				String arg_type = StackBuffer.typeSupported(parts[pos].trim());
@@ -89,31 +109,52 @@ public class RPCStubGenerator
 					continue;
 				}
 				
-				arg_list.append(arg_type + " arg" + arg_num);
+				prototype.append(arg_type + " arg" + arg_num);
 				arg_types[arg_num] = arg_type;
+				callee_args.append("arg" + arg_num);
 			}
-			
-			arg_list.append(")");
+			prototype.append(")");
+			callee_args.append(")");
 			
 			/* Write the header to all 3 files */
-			interface_file.println(header.toString() + arg_list.toString() + ";");
-			caller_stubs.println(header.toString() + arg_list.toString());
-			callee_stubs.println(callee_header.toString());
+			interface_file.println(header.toString() 
+					+ prototype.toString() + ";");
+			caller_stubs.println(header.toString() + prototype.toString());
+			callee_stubs.println(callee_header.toString() 
+					+ "(StackBuffer stack)");
 			
-			/* Generate the caller stubs */
+			/* Generate the callee function entry */
 			caller_stubs.println("\t{");
 			caller_stubs.println("\t\tStackBuffer stack = new StackBuffer();");
 			caller_stubs.println("");
 			caller_stubs.println("\t\t/* Push the function number */");
 			caller_stubs.println("\t\tstack.pushInt(" + func_num + ");");
 			caller_stubs.println("\t\t/* Push the arguments */");
+			
+			/* Generate the callee function entry */
+			callee_stubs.println("\t{");
+			callee_stubs.println("\t\t/* Pop the arguments */");
+			
+			/* Generate the start of a handler case */
+			handler.println("\t\t\tcase " + func_num 
+					+ ": /** " + func_name + " */");
+			handler.println("\t\t\t\tresult = __" + func_name + "(stack);");
+			handler.println("\t\t\t\tbreak;");
+			
+			/* Do any needed pushing or popping off of the stack */
 			for(int x = 0;x < arg_types.length;x++)
 			{
 				String arg_type = arg_types[x];
+				/* Caller arguments */
 				caller_stubs.println("\t\t" 
 						+ StackBuffer.getPushMethod(arg_type) 
 						+ "(arg" + x + ");");
+				/* Callee arguments */
+				callee_stubs.println("\t\t" + arg_type
+						+ " arg" + x + " = stack."
+						+ StackBuffer.getPopMethod(arg_type) + "();");
 			}
+			
 			caller_stubs.println("\t\t/* Do the network call */");
 			caller_stubs.println("\t\tStackBuffer res = rpc.do_call(stack);");
 			caller_stubs.println("\t\treturn res." 
@@ -121,9 +162,20 @@ public class RPCStubGenerator
 			caller_stubs.println("\t}");
 			caller_stubs.println("");
 			
+			callee_stubs.println("");
+			callee_stubs.println("\t\t/* Do the function call */");
+			callee_stubs.println("\t\treturn " + callee_receiver_object 
+					+ func_name  + callee_args.toString() + ";");
+			callee_stubs.println("\t}");
+			
 			line++;
 			func_num++;
 		}
+		
+		handler.println("\t\t\tdefault:");
+		handler.println("\t\t\t\tinvalid_rpc(func_num);");
+		handler.println("\t\t\t\tbreak;");
+		handler.println("\t\t};");
 		
 		file.close();
 		interface_file.close();
